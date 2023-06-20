fc00c9fc65e4301ffff7d5e59b8f529a4ce1884d
Julian Quispe <julianmql@hotmail.com>
2023-06-20 12:10:41 -0500

    feat: waiting room page added

---
+++ pages/waitingRoom.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import UsuarioBC from  "../BC/usuarioBC";
+import { CardsCarousel } from '../components/CardsCarousel';
+import { GridDisplay } from '../components/GridDisplay';
+import { HeroTicket } from '../components/HeroTicket';
+import { HeroInfo } from '../components/HeroInfo';
+import { HeroPrices } from '../components/HeroPrices';
+import { HeroConditions } from '../components/HeroConditions';
+import type { ReactElement } from 'react'
+import type { NextPageWithLayout } from './_app'
+import { ApolloProvider } from '@apollo/react-hooks';
+import Layout from '../components/Layout';
+import cliente from '../graphql/cliente';
+import { AuthenticationImage } from '../components/LoginView';
+import { WaitingImage } from '../components/WaitingRoomView';
+
+const WaitingRoomPage: NextPageWithLayout = () => {
+  return (
+        <WaitingImage></WaitingImage>
+  );
+}
+
+WaitingRoomPage.getLayout = function getLayout(page: ReactElement){
+  return (
+    <ApolloProvider client={cliente}>
+        {page}
+    </ApolloProvider>
+  )
+}
+
+
+export default WaitingRoomPage
